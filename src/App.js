7c54e50b986cda79b4556502bfb39237cc7ab870
neil.garner <neil.garner@autotrader.co.uk>
2020-02-17 15:02:59 +0000

    graphql stuff

--- src/App.js
+++ src/App.js
@@ -1,56 +1,61 @@
-import React, { Component } from "react"
-import logo from "./logo.svg"
-import "./App.css"
-import ApolloClient from "apollo-boost";
+import React, { Component } from 'react'
+import logo from './logo.svg'
+import './App.css'
+import { ApolloProvider } from '@apollo/react-hooks'
+import ApolloClient from 'apollo-boost'
+import { GraphQLTest } from './GraphQLTest'
 
 const client = new ApolloClient({
-    uri: "/.netlify/functions/graphql"
-});
-console.log("​client", client)
+    uri: '/.netlify/functions/graphql'
+})
+console.log('​client', client)
 
 class LambdaDemo extends Component {
-  constructor(props) {
-    super(props)
-    this.state = { loading: false, msg: null }
-  }
+    constructor(props) {
+        super(props)
+        this.state = { loading: false, msg: null }
+    }
 
-  handleClick = api => e => {
-    e.preventDefault()
+    handleClick = api => e => {
+        e.preventDefault()
 
-    this.setState({ loading: true })
-    fetch("/.netlify/functions/" + api)
-      .then(response => response.json())
-      .then(json => this.setState({ loading: false, msg: json.msg }))
-  }
+        this.setState({ loading: true })
+        fetch('/.netlify/functions/' + api)
+            .then(response => response.json())
+            .then(json => this.setState({ loading: false, msg: json.msg }))
+    }
 
-  render() {
-    const { loading, msg } = this.state
+    render() {
+        const { loading, msg } = this.state
 
-    return (
-      <p>
-        <button onClick={this.handleClick("hello")}>{loading ? "Loading..." : "Call Lambda"}</button>
-        <button onClick={this.handleClick("async-dadjoke")}>{loading ? "Loading..." : "Call Async Lambda"}</button>
-        <br />
-        <span>{msg}</span>
-      </p>
-    )
-  }
+        return (
+            <ApolloProvider client={client}>
+                <button onClick={this.handleClick('hello')}>{loading ? 'Loading...' : 'Call Lambda'}</button>
+                <button onClick={this.handleClick('async-dadjoke')}>
+                    {loading ? 'Loading...' : 'Call Async Lambda'}
+                </button>
+                <br />
+                <span>{msg}</span>
+                <GraphQLTest />
+            </ApolloProvider>
+        )
+    }
 }
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <LambdaDemo />
-        </header>
-      </div>
-    )
-  }
+    render() {
+        return (
+            <div className='App'>
+                <header className='App-header'>
+                    <img src={logo} className='App-logo' alt='logo' />
+                    <p>
+                        Edit <code>src/App.js</code> and save to reload.
+                    </p>
+                    <LambdaDemo />
+                </header>
+            </div>
+        )
+    }
 }
 
 export default App
---
+++ src/GraphQLTest.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import { useQuery } from '@apollo/react-hooks';
+import { gql } from 'apollo-boost';
+
+const QUERY = gql`
+  {
+    hello
+  }
+`;
+
+export const GraphQLTest = () => {
+    const { loading, error, data } = useQuery(QUERY);
+    if (loading) return <p>Loading...</p>;
+    if (error) return <p>Error :(</p>;
+        
+    console.log("​GraphQLTest -> data", data)
+    
+    return (
+        <>
+            <p></p>
+        </>
+    )
+}
