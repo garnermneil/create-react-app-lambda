1b5e4a5b4289c7d5064b122fd2faf1ef2db3eea2
neil.garner <neil.garner@autotrader.co.uk>
2020-02-17 16:41:12 +0000

    Tryout

--- src/lambda/graphql.js
+++ src/lambda/graphql.js
@@ -1,8 +1,39 @@
 const { ApolloServer, gql } = require("apollo-server-lambda");
 
+const LETTINGS = [{
+    id: 'A001',
+    name: 'One',
+    price: 200,
+    location: {
+        latitude: 1,
+        longitude: 1
+    }
+},
+{
+    id: 'A002',
+    name: 'Two',
+    price: 300,
+    location: {
+        latitude: 1,
+        longitude: 1
+    }
+}]
+
 const typeDefs = gql`
   type Query {
     hello: String
+  }
+
+  type Letting {
+      id: String!
+      name: String!
+      price: Float!
+      location: Location!
+  }
+
+  type Location {
+      latitude: Float!
+      longitude: Float!
   }
 `;
 
@@ -10,6 +41,10 @@ const resolvers = {
   Query: {
     hello: (parent, args, context) => {
       return "Hello, world!";
+    },
+
+    lettings: (parent, arg, context) => {
+        return LETTINGS
     }
   }
 };
